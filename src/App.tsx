04c64411a8676f3274d96fbc79a093e6936be755
Haki-Malai <p17mala@ionio.gr>
2023-04-03 12:51:39 +0300

    feat(App.tsx): add list of cars to App component header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
 function App() {
+  const cars = [
+    {title: "Toyota", value: "toyota"},
+    {title: "Honda", value: "honda"},
+    {title: "Ford", value: "ford"},
+    {title: "Chevrolet", value: "chevrolet"},
+    {title: "Nissan", value: "nissan"},
+    {title: "BMW", value: "bmw"},
+    {title: "Audi", value: "audi"},
+    {title: "Tesla", value: "tesla"}
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello world!</h1>
+        {cars.map((car) => <h1>{car.title}</h1>)}
       </header>
     </div>
   );
